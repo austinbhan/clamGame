b04a4a4302f301f49705a04bf1a247737101184c
austinbhan <austinbhan@outlook.com>
2022-04-25 17:59:02 -0700

    Random number generated for one button

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 // import functions and grab DOM elements
-const shellder = document.getElementById('shellder');
-const blueShell = document.getElementById('blueShell');
-const spongeBob = document.getElementById('spongeBob');
+const shell1 = document.getElementById('shell1');
+const shell2 = document.getElementById('shell2');
+const shell3 = document.getElementById('shell3');
 
-const shellderBtn = document.getElementById('shellderBtn');
-const blueShellBtn = document.getElementById('blueShellBtn');
-const spongeBobBtn = document.getElementById('spongeBobBtn');
+const shell1Btn = document.getElementById('shell1Btn');
+const shell2Btn = document.getElementById('shell2Btn');
+const shell3Btn = document.getElementById('Shell3Btn');
 
-let wins = document.getElementById('winSpan');
-let total = document.getElementById('totalSpan');
-let losses = document.getElementById('lossSpan');
+const winSpan = document.getElementById('winSpan');
+const totalSpan = document.getElementById('totalSpan');
+const lossSpan = document.getElementById('lossSpan');
 
 // let state
 
+let wins = 0;
+let total = 0;
+let losses = 0;
+
 // set event listeners 
+
+shell1Btn.addEventListener('click', () => {
+  const randomShell = Math.ceil(Math.random() * 3);
+  console.log('the random shell is', randomShell);
+});
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
